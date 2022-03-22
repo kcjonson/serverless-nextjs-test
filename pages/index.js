8325ceb996a3388fa06b7edc01cb611b77beaf09
Kevin Jonson <kevin.jonson@perchwell.com>
2022-03-22 11:13:23 -0700

    something is seriously buggered

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js 8!</a>
+          Welcome to <a href="https://nextjs.org">Next.js 9!</a>
         </h1>
 
         <p className={styles.description}>
